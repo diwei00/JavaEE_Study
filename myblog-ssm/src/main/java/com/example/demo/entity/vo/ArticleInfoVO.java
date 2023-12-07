7a30540d3b06365108c44c7ac32d02cc8c868f03
wuhao <2945608334@qq.com>
2023-12-07 16:32:40 +0800

    更新myblog-ssm项目

--- myblog-ssm/src/main/java/com/example/demo/entity/vo/ArticleInfoVO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity.vo;
-
-import com.example.demo.entity.ArticleInfo;
-import lombok.Data;
-
-@Data
-public class ArticleInfoVO extends ArticleInfo {
-    private String username;
-}
